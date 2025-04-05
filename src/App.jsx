d7b33f43a7d9f754a24a3514dbecd5c97f5f7d68
Eray Özdemir <eray005756@gmail.com>
2025-04-06 02:06:22 +0300

    Fixed line endings for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
 
 import BackGroundCase from "./Components/BackGroundCase";
-import Container from "./Components/container";
 
 function App() {
   return (
     <>
-      <BackGroundCase>
-        <Container></Container>
-      </BackGroundCase>
+      <BackGroundCase></BackGroundCase>
     </>
   );
 }
